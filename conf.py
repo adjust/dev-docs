ee37455da951a136f0d4823e46aa3bafaa329353
Ciarán Ainsworth <ciaran.ainsworth@adjust.com>
2023-02-08 00:39:11 +0100

    Remove absolute paths

--- conf.py
+++ conf.py
@@ -14,11 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('./fragments'))
-
 
 # -- Project information -----------------------------------------------------
 
