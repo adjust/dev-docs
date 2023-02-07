b37fba1ce6b32d06d9391e7a3cad0820884a6bfd
Ciarán Ainsworth <ciaran.ainsworth@adjust.com>
2023-02-07 19:57:34 +0100

    Add basic picker logic

--- serve.py
+++ serve.py
@@ -2,9 +2,9 @@
 from subprocess import call
 
 # initial make
-call(["poetry", "run", "sphinx-build", ".", "/tmp/_build"])
-from livereload import Server, shell  # noqa: E402
+call(["poetry", "run", "sphinx-multiversion", ".", "/tmp/_build"])
+from livereload import Server, shell
 
 server = Server()
-server.watch(".", shell("poetry run sphinx-build . /tmp/_build"))
+server.watch(".", shell("poetry run sphinx-multiversion . /tmp/_build"))
 server.serve(root="/tmp/_build/", liveport=35730, port=8001, host="0.0.0.0")
--- unity/index.md
+++ unity/index.md
@@ -1,3 +1,8 @@
 # Get started
 
 The Adjust Unity SDK enables you to measure attribution, events, and more in your Unity app. Follow the steps in this guide to set up your app to work with the Adjust SDK.
+
+:::{important}
+The Adjust SDK supports iOS 9 or later and Android API level 9 (Gingerbread) or later.
+:::
+
