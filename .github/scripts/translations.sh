1e003bc51babfe84b10b9ed7f7f1e88f50883d51
Ciarán Ainsworth <ciaran.ainsworth@adjust.com>
2023-02-09 12:07:27 +0100

    Fix CI checks

--- .github/scripts/translations.sh
+++ .github/scripts/translations.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Run from the docs directory
+cd docs
+
+# Generate the new translation files
 poetry run make gettext
 
+# Go through each directory and update with any changes
 for path in locale/*; do
   lang="$(basename "$path")"
   poetry run sphinx-intl update -l "$lang"
