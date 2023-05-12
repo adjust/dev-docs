19aa27a63160445958e51a72999a1d6d138a6831
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-05-12 13:56:25 +0200

    Add pre-commit configuration and install

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 HOMEBREW_INSTALLED=false
 PYTHON_INSTALLED=false
 POETRY_INSTALLED=false
+VALE_INSTALLED=false
+PRECOMMIT_INSTALLED=false
 POETRY_IN_PATH=false
 
 ARCH=$(uname -m)
@@ -82,9 +84,53 @@ fi
 
 sleep .5;
 
+if ! [ $HOMEBREW_INSTALLED == true ]; then
+   printf "\nHomebrew is required to install Vale."; exit 1;
+elif ! command -v vale &> /dev/null; then
+   printf "\nVale not installed. "
+   while true; do
+      read -p "Do you want to install this program? Answer 'y' or 'n': " yn
+      case $yn in
+         [Yy]* ) printf "\nInstalling now... "; brew install vale; VALE_INSTALLED=true; printf "Vale successfully installed!"; break;;
+         [Nn]* ) break;;
+         * ) printf "\nPlease answer 'y' or 'n'.";;
+      esac
+   done
+else
+    printf "\nVale installed via Homebrew."
+    VALE_INSTALLED=true
+fi
+
+if ! [ $HOMEBREW_INSTALLED == true ]; then
+   printf "\nHomebrew is required to install pre-commit."; exit 1;
+elif ! command -v pre-commit &> /dev/null; then
+   printf "\npre-commit not installed. "
+   while true; do
+      read -p "Do you want to install this program? Answer 'y' or 'n': " yn
+      case $yn in
+         [Yy]* ) printf "\nInstalling now... "; brew install pre-commit; PRECOMMIT_INSTALLED=true; printf "pre-commit successfully installed!"; break;;
+         [Nn]* ) break;;
+         * ) printf "\nPlease answer 'y' or 'n'.";;
+      esac
+   done
+else
+    printf "\npre-commit installed via Homebrew."
+    PRECOMMIT_INSTALLED=true
+fi
+
+if ! [ $PRECOMMIT_INSTALLED == true ]; then
+   printf "\npre-commit is required to install pre-commit hooks"; break;
+else
+   printf "\nInstalling pre-commit hooks... "; pre-commit install --install-hooks; printf "pre-commit hooks successfully installed!"; break;
+fi
+
+printf "\n\n"
+printf -- '-%.0s' {1..100}; echo ""
 printf "\nHomebrew installed? "; sleep .5; printf "$HOMEBREW_INSTALLED"
 printf "\nPython installed? "; sleep .5; printf "$PYTHON_INSTALLED"
 printf "\nPoetry installed? "; sleep .5; printf "$POETRY_INSTALLED"
+printf "\nVale installed? "; sleep .5; printf "$VALE_INSTALLED"
+printf "\npre-commit installed? "; sleep .5; printf "$PRECOMMIT_INSTALLED"
 printf "\nPoetry available in PATH? "; sleep .5; printf "$POETRY_IN_PATH"
 
 if ! ($HOMEBREW_INSTALLED && $PYTHON_INSTALLED && $POETRY_INSTALLED && $POETRY_IN_PATH); then
