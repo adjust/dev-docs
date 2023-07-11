6f63ac2abf8602d9c7c281939397735a047fd9d1
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-07-11 14:54:22 +0300

    feat: added env example

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,12 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-	readonly GITHUB_TOKEN: string | undefined;
+  readonly GITHUB_TOKEN: string | undefined;
+  readonly ALGOLIA_INDEX_NAME: string | undefined;
+  readonly ALGOLIA_APP_ID: string | undefined;
+  readonly ALGOLIA_API_KEY: string | undefined;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
