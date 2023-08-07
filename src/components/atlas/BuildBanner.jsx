198dce0703b395ad7e4785becdc2e95988567989
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-08-07 09:46:39 +0200

    feat: add inline documentation

--- src/components/atlas/BuildBanner.jsx
+++ src/components/atlas/BuildBanner.jsx
@@ -2,6 +2,9 @@
 import { Banner } from "@adjust/components";
 
 export default function BuildBanner(props) {
+  // The Atlas component passes the body content as a string of HTML.
+  // We convert this to HTML using the `dangerouslySetInnerHTML` function.
+
   const description = (
     <div dangerouslySetInnerHTML={{ __html: props.description }} />
   );
