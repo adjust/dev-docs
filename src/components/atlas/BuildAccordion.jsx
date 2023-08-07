f9eecfa14093eec2ebd3fc999bab51993410d173
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-08-07 12:09:59 +0200

    feat: Add accordion element and kitchen sink

---
+++ src/components/atlas/BuildAccordion.jsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource react */
+import { Accordion } from "@adjust/components";
+
+// We only need one type of Accordion currently.
+// This Accordion should contain a title, a body, and an optional badge
+
+export default function BuildAccordion(props) {
+  // The Atlas component passes the body content as a string of HTML.
+  // We convert this to HTML using the `dangerouslySetInnerHTML` function.
+
+  const content = <div dangerouslySetInnerHTML={{ __html: props.content }} />;
+
+  // Each accordion exists in its own list, so we can hardcode the value of `id` to 1.
+  // If we want to create multi-accordion lists in future we will need to iterate the `id`.
+
+  const data = [
+    {
+      id: 1,
+      title: props.title,
+      content: content,
+    },
+  ];
+
+  // Only add a badge if `props.badge` contains a value
+
+  if (props.badge) {
+    data[0].badge = { label: props.badge, color: "neutral" };
+  }
+
+  return <Accordion data={data} type="headline" />;
+}
