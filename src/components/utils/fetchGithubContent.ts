5da396a0f392919bcea0205b8ef72c6b434d8951
Ciarán Ainsworth <ciaranainsworth@gmail.com>
2023-08-14 21:43:18 +0200

    chore: add error message for missing GH token

--- src/components/utils/fetchGithubContent.ts
+++ src/components/utils/fetchGithubContent.ts
@@ -4,12 +4,19 @@ The content of the response is a Base64-encoded version of the file specified. T
 */
 
 type GitHubContent = Omit<ApiObject, "range">;
+const token: string | undefined = import.meta.env.GITHUB_TOKEN;
+
+if (!token) {
+  throw new Error(
+    `Missing GitHub API token. Check the .env.sample file for information.`,
+  );
+}
 
 export async function fetchGithubContent(repoInfo: GitHubContent) {
   const baseURL = `https://api.github.com/repos/${repoInfo.orgName}/${repoInfo.repoName}/contents/${repoInfo.file}?ref=${repoInfo.ref}`;
   const response = await fetch(baseURL, {
     method: "GET",
-    headers: { Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}` },
+    headers: { Authorization: `Bearer ${token}` },
   }).catch((err) => {
     throw new Error(`Error fetching content from ${baseURL}. ${err}`);
   });
@@ -19,6 +26,6 @@ export async function fetchGithubContent(repoInfo: GitHubContent) {
   }
 
   let body = await response.json();
-  // Content body from github is base64 encoded
+  // Decode Base64-encoded body and return it
   return atob(body.content);
 }
