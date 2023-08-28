d9e4bf6e7b9deb98673453d7c254f178e76aab80
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-08-28 17:21:09 +0300

    feat: added new package, added tabStore

--- src/components/TabComponent/index.tsx
+++ src/components/TabComponent/index.tsx
@@ -1,11 +1,28 @@
 /** @jsxImportSource react */
-import type { FC } from "react";
+import { useStore } from "@nanostores/react";
+import { useEffect, type FC, useState } from "react";
+import classNames from "classnames";
+
+import { $tabs } from "@store/tabStore";
 
 const TabComponent: FC<{
   content: string;
   sync: string;
 }> = (props) => {
-  return <div dangerouslySetInnerHTML={{ __html: props.content }} />;
+  const tabs = useStore($tabs);
+  // for some reason, this component don`t normally render with the default value so we need to use additional store
+  const [currentTab, setCurrentTab] = useState(props.sync);
+
+  useEffect(() => {
+    setCurrentTab(tabs.currentSync);
+  }, [tabs.currentSync]);
+
+  return (
+    <div
+      className={classNames("block", { hidden: props.sync !== currentTab })}
+      dangerouslySetInnerHTML={{ __html: props.content }}
+    />
+  );
 };
 
 export default TabComponent;
--- src/components/atlas/BuildTabs.tsx
+++ src/components/atlas/BuildTabs.tsx
@@ -1,21 +1,34 @@
 /** @jsxImportSource react */
-import { useState, type FC } from "react";
-import { htmlWithTitles } from "@components/utils/htmlWithTitles";
+import { type FC, useEffect } from "react";
 import { Tabs } from "@adjust/components";
+import { useStore } from "@nanostores/react";
+
+import { htmlWithTitles } from "@components/utils/htmlWithTitles";
+import { $tabs, changeCurrentTab, updateTabs } from "@store/tabStore";
+
 import type { TabItemType } from "@adjust/components/build/Tabs/TabItem";
 
 const BuildTabs: FC<{
   content: string;
   items: TabItemType[];
 }> = (props) => {
-  const [currentTab, setCurrentTab] = useState("1");
+  const tabs = useStore($tabs);
+
   /* The Astro component passes the body content as a string of HTML.
   We use a helper function to convert this into usable HTML content*/
   const content = htmlWithTitles(props.content);
 
+  useEffect(() => {
+    updateTabs(props.items);
+  }, []);
+
   return (
     <>
-      <Tabs items={props.items} value={currentTab} onChange={setCurrentTab} />
+      <Tabs
+        items={props.items}
+        value={tabs.currentSync}
+        onChange={changeCurrentTab}
+      />
       {content.body}
     </>
   );
