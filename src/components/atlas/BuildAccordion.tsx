67796bc2fa16167f0dcc9a2318512ae1f9f1ff33
Ciarán Ainsworth <sporiff@tilde.club>
2023-09-05 13:49:42 +0200

    feat: simplify Accordion component

--- src/components/atlas/BuildAccordion.tsx
+++ src/components/atlas/BuildAccordion.tsx
@@ -1,39 +1,20 @@
 /** @jsxImportSource react */
 import { Accordion } from "@adjust/components";
+import type { FC, ReactNode } from "react";
 import type { AccordionItem } from "@adjust/components/build/Accordion/Item";
-import { htmlWithTitles } from "@components/utils/htmlWithTitles";
-import type { FC } from "react";
-
-// We only need one type of Accordion currently.
-// This Accordion should contain a title, a body, and an optional badge
 
 const BuildAccordion: FC<{
-  content: string;
+  data: AccordionItem[];
+  content?: ReactNode;
 }> = (props) => {
-  // The Atlas component passes the body content as a string of HTML.
-  // We convert this to HTML using the `dangerouslySetInnerHTML` function.
+  let AccordionData = props.data;
 
-  const content = htmlWithTitles(props.content);
-
-  const data: AccordionItem[] = [
-    {
-      id: content.anchor,
-      title: content.title || "",
-      content: content.body,
-      badge: {},
-    },
-  ];
-
-  // Only add a badge if `props.badge` contains a value
-
-  if (content.badge) {
-    data[0].badge = { label: content.badge, color: "neutral" };
-  }
+  AccordionData[0].content = props.content;
 
   return (
-    <div id={content.anchor}>
-      <Accordion data={data} type="headline" />
-    </div>
+    <>
+      <Accordion data={AccordionData} type="headline" />
+    </>
   );
 };
 
