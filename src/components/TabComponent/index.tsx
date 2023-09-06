6930a1ee447a9c6d46d4d9963047705fe69e6079
Ciarán Ainsworth <sporiff@tilde.club>
2023-09-06 10:41:58 +0200

    refactor: use html children in tabs

--- src/components/TabComponent/index.tsx
+++ src/components/TabComponent/index.tsx
@@ -4,17 +4,13 @@ import type { FC } from "react";
 
 
 const TabComponent: FC<{
-  content: string;
+  content: React.ReactElement;
   uniqueId: string;
-}> = ({content, uniqueId}) => {
-
+}> = ({ content, uniqueId }) => {
   return (
     <div id={uniqueId} className="hidden">
       {/* additional div in case of adding styles to the content */}
-      <div
-        id="tab-content"
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <div id="tab-content" dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 };
