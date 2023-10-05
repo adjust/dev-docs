ff0522d47e193c0de519fa6a1c567e99384a2fb9
Ciarán Ainsworth <ciaranainsworth@proton.me>
2023-10-05 14:19:47 +0200

    feat(docs): add initial iOS config docs

--- src/components/utils/convertCase.ts
+++ src/components/utils/convertCase.ts
@@ -1,5 +1,8 @@
 export function toSentenceCase(string: string): string {
   let lower = string.toLowerCase();
+  if (lower === "seealso") {
+    lower = "see also"
+  }
   return lower[0].toUpperCase() + lower.slice(1);
 }
 
