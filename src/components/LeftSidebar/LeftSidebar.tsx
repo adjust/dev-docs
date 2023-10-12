6c90854a2f2779e5d8b5275a7b08bd612a7e4890
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-10-12 13:36:19 +0300

    feat: refactoring

--- src/components/LeftSidebar/LeftSidebar.tsx
+++ src/components/LeftSidebar/LeftSidebar.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 
 import { getLanguageFromURL } from "../../languages";
 import type { getNavigationEntries } from "../../utils/helpers/navigation/getNavigationEntries";
-import LeftSidebarItem from "./LeftSidebarItem";
+import NavigationItem from "./NavigationItem";
 
 type Props = {
   currentPage: string;
@@ -21,6 +21,7 @@ const LeftSidebar: FC<Props> = ({ currentPage, navigationEntries }) => {
 
   return (
     <div className="xs:h-screen lg:h-[calc(100vh-86px)] lg:sticky lg:top-[86px] border-r-[1.5px] border-[#E0EAFF] bg-secondary">
+      {/* Navigation */}
       <nav
         id="sidebar"
         className="w-[294px] break-words lg:flex flex-col gap-y-[18px] pr-6 text-secondary  lg:h-[calc(100%-62px)] lg:pb-6 lg:pl-[18px] lg:pt-12 xs:pt-4 xs:h-full lg:static xs:fixed xs:inset-y-0 xs:left-0 xs:z-30 xs:shadow-right xs:pl-3 lg:shadow-none overflow-y-auto print:hidden"
@@ -32,7 +33,7 @@ const LeftSidebar: FC<Props> = ({ currentPage, navigationEntries }) => {
               "nav_top-category pb-4": currentPage === child.slug,
             })}
           >
-            <LeftSidebarItem
+            <NavigationItem
               currentPage={currentPageMatch}
               sidebarData={child}
               level={child.level}
