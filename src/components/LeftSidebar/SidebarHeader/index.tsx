3a255989fb1af34589e5146f155afc962281584b
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-10-16 18:11:11 +0300

    fix: fixed header styles, added home link

--- src/components/LeftSidebar/SidebarHeader/index.tsx
+++ src/components/LeftSidebar/SidebarHeader/index.tsx
@@ -1,10 +1,14 @@
+import type { FC } from "react";
+
 import AdjustLogo from "@components/Icons/react/AdjustLogo";
 import AudienceDropdown from "./AudienceDropdown";
 
-const SidebarHeader = () => {
+const SidebarHeader: FC<{ homeUrl: string }> = ({ homeUrl }) => {
   return (
     <div className="sidebar-header flex flex-row items-center mb-[74px] pt-7 pl-[27px]">
-      <AdjustLogo />
+      <a href={homeUrl}>
+        <AdjustLogo />
+      </a>
       <AudienceDropdown />
     </div>
   );
