6b053eaf2be0bfa656b27f82a81f2dc2ffb462c6
Ciarán Ainsworth <ciaranainsworth@proton.me>
2023-10-24 18:10:04 +0200

    feat(docs): Add tooltip

---
+++ src/components/atlas/BuildTooltip.tsx
@@ -0,0 +1,23 @@
+import { Position, Tooltip } from "@adjust/components";
+import type { FC, ReactNode, ReactElement } from "react";
+
+const BuildTooltip: FC<{
+  content: ReactNode;
+  children: ReactElement;
+  position?: Position;
+}> = (props) => {
+  console.log(props);
+  return (
+    <>
+      <Tooltip
+        position={props.position ? props.position : "top"}
+        content={props.content}
+        zIndex={100}
+      >
+        <span className="underline decoration-dashed">{props.children}</span>
+      </Tooltip>
+    </>
+  );
+};
+
+export default BuildTooltip;
