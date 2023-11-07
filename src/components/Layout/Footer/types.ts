97f91cab41f1bc7e5f470751265b11663c8d1367
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-11-07 12:42:03 +0200

    feat: added content footer

--- src/components/Layout/Footer/types.ts
+++ src/components/Layout/Footer/types.ts
@@ -1,20 +1,6 @@
 import type { Locales } from "@i18n/locales";
 import type { IWithLanguage } from "src/types/WithLanguage";
 
-export interface FooterProps extends IWithLanguage {
-  /** If true we display a contact support block  */
-  withSupport?: boolean;
-  narrow?: boolean;
-  /** If true we do not display the language dropdown and we have another text in the Contact support block */
-  contentLevel?: boolean;
-}
-
-export interface FooterMainProps extends IWithLanguage {
-  /** If true we display a contact support block  */
-  withSupport?: boolean;
-  narrow?: boolean;
-}
-
 export interface LanguageSwitchProps {
   lang: string;
   locales: Locales;
