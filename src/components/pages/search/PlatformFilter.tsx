c2ca6f8dd2f62e0b896083c31d5792e71ec19a4e
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-11-08 16:04:05 +0200

    feat: more progress with search page layout

---
+++ src/components/pages/search/PlatformFilter.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import { FilterButton } from "@adjust/components";
+
+const platforms = [
+  { label: "All", value: "all" },
+  { label: "IOS", value: "ios" },
+  { label: "Flutter", value: "flutter" },
+  { label: "React Native", value: "reactNative" },
+  { label: "Unity", value: "unity" },
+  { label: "Web", value: "web" },
+];
+const PlatformFilter = () => {
+  const [selectedItems, setSelectedItems] = useState([platforms[0]]);
+
+  return (
+    <FilterButton
+      name="Platform"
+      multiple={false}
+      items={platforms}
+      selectedItems={selectedItems}
+      onApply={setSelectedItems}
+    />
+  );
+};
+
+export default PlatformFilter;
