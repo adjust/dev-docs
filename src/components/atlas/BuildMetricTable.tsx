0ec503447c5e43e7ee7635ffb03365db1558d8c0
Ciarán Ainsworth <ciaranainsworth@proton.me>
2023-11-09 16:07:22 +0100

    feat(docs): add logic for sorting by section

--- src/components/atlas/BuildMetricTable.tsx
+++ src/components/atlas/BuildMetricTable.tsx
@@ -1,13 +1,12 @@
-/** @jsxImportSource react */
-import { TableV2, Input } from "@adjust/components";
+import { TableV2, Input, ActionMenu, IconButton } from "@adjust/components";
 import type { FC } from "react";
-import type { TableColumnTypes } from "@adjust/components";
+import type { ItemType, TableColumnTypes } from "@adjust/components";
 import { useState } from "react";
+import Markdown from "react-markdown";
 
 // Create an interface for the cells. These can be string pairs of any accessor and value.
 
 const BuildMetricTable: FC<{
-  columns: TableColumnTypes[];
   data: ColumnCell[];
   search: boolean;
   resizable: boolean;
@@ -17,11 +16,81 @@ const BuildMetricTable: FC<{
   height?: TableHeights;
 }> = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState(props.data);
 
   const visualProperties = {
     areColumnsBordered: props.bordered,
     isStriped: props.striped,
   };
+
+  const sections = [
+    ...new Set(props.data.map((item) => item.section).filter((n) => n)),
+  ];
+
+  const SectionFilters = () => {
+    let items: ItemType[] = [
+      { type: "divider" },
+      {
+        label: "Clear",
+        iconName: "Cross",
+        onClick: () => {
+          setData(props.data);
+        },
+      },
+    ];
+
+    sections.forEach((section) => {
+      let item: ItemType = {
+        label: section,
+        onClick: () => {
+          setData(props.data.filter((i) => i.section === section));
+        },
+      };
+      items.unshift(item);
+    });
+
+    return (
+      <ActionMenu items={items}>
+        <IconButton iconName="Filter" aria-label="Filter" size="small" />
+      </ActionMenu>
+    );
+  };
+
+  const columns: TableColumnTypes[] = [
+    {
+      Header: "Metric",
+      accessor: "metric",
+      Cell: ({ value }) => (
+        <Markdown
+          components={{
+            p: "code",
+          }}
+        >
+          {value}
+        </Markdown>
+      ),
+    },
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Data type",
+      accessor: "data_type",
+      disableGlobalFilter: true,
+    },
+    {
+      Header: "Section",
+      accessor: "section",
+      sortable: true,
+      headerActions: <SectionFilters />,
+    },
+    {
+      Header: "Description",
+      accessor: "description",
+      Cell: ({ value }) => <Markdown>{value}</Markdown>,
+    },
+  ];
 
   return (
     <div className="overflow-auto">
@@ -41,8 +110,8 @@ const BuildMetricTable: FC<{
         </div>
       )}
       <TableV2
-        data={props.data}
-        columns={props.columns}
+        data={data}
+        columns={columns}
         searchTerm={searchTerm}
         visualProperties={visualProperties}
         flex={props.flex}
