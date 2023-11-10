e8be3d95c61608d5dd93a712c0c145a6c9956328
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-11-10 12:20:38 +0200

    feat: added placeholder articles

---
+++ src/components/pages/search/static.ts
@@ -0,0 +1,51 @@
+// placeholders for the search page. Will be removed when logic for search is added
+export const SEARCH_PLACEHOLDER_ARTICLES = [
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [
+      { title: "Breadcrumb 1", link: "/en" },
+      { title: "Breadcrumb 2", link: "/en" },
+    ],
+  },
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [
+      { title: "Breadcrumb 1", link: "/en" },
+      { title: "Breadcrumb 2", link: "/en" },
+      { title: "Breadcrumb 3", link: "/en" },
+    ],
+  },
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [{ title: "Breadcrumb 1", link: "/en" }],
+  },
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [
+      { title: "Breadcrumb 1", link: "/en" },
+      { title: "Breadcrumb 2", link: "/en" },
+      { title: "Breadcrumb 3", link: "/en" },
+      { title: "Breadcrumb 4", link: "/en" },
+    ],
+  },
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [
+      { title: "Breadcrumb 1", link: "/en" },
+      { title: "Breadcrumb 2", link: "/en" },
+    ],
+  },
+  {
+    title: "Article Headline",
+    description: `​adjust's dynamic Tracker URL system allows you to measure the conversions made from all of your click based inventory and view-through campaigns.`,
+    breadcrumbs: [
+      { title: "Breadcrumb 1", link: "/en" },
+      { title: "Breadcrumb 2", link: "/en" },
+    ],
+  },
+];
