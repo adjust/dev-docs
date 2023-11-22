33acdeee0f1f17b11dd1f7a9115f629cbee567fc
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-11-22 12:16:06 +0200

    fix: fixed margins for the "no results" state

--- src/components/pages/search/SearchNoResults.tsx
+++ src/components/pages/search/SearchNoResults.tsx
@@ -4,7 +4,7 @@ import { HELP_CENTER_LINK } from "src/consts";
 
 const SearchNoResults: FC<{ bodyText: string }> = ({ bodyText }) => {
   return (
-    <div className="flex flex-col items-center w-full xs:pl-4 lg:pl-0  bg-[#F4F7FD]">
+    <div className="flex flex-col items-center w-full xs:pl-4 lg:pl-0  bg-[#F4F7FD] my-6">
       <div className="">
         <img src="/images/search/no-results-search.svg" />
       </div>
