d08535a2d5cbd4f5a8867c448ca09d365cdd59a7
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-11-22 14:07:38 +0200

    feat: added search params to the HC search link

--- src/components/pages/search/SearchNoResults.tsx
+++ src/components/pages/search/SearchNoResults.tsx
@@ -1,8 +1,12 @@
 import type { FC } from "react";
 
 import { HELP_CENTER_LINK } from "src/consts";
+import { getSearchParams } from "./utils";
 
 const SearchNoResults: FC<{ bodyText: string }> = ({ bodyText }) => {
+  const { query } = getSearchParams();
+  const helpCenterParams = `query=${query}&page=1&limit=15`;
+
   return (
     <div className="flex flex-col items-center w-full xs:pl-4 lg:pl-0  bg-[#F4F7FD] my-6">
       <div className="">
@@ -11,7 +15,7 @@ const SearchNoResults: FC<{ bodyText: string }> = ({ bodyText }) => {
       <div className="text-[32px] text-primary pb-2">No results</div>
       <div className="text-secondary mb-4">{bodyText}</div>
       <a
-        href={`${HELP_CENTER_LINK}en/search`}
+        href={`${HELP_CENTER_LINK}en/search?${helpCenterParams}`}
         className="flex items-center  p-4 h-10 w-fit rounded-md font-semibold bg-white hover:bg-slate-200"
       >
         Open Help Center
