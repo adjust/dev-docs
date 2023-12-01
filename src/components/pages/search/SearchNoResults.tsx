1ae29d4e80b68bcf643d6199cf6fc7ec899252c1
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2023-12-01 16:59:10 +0200

    fix: fixes for the search page

--- src/components/pages/search/SearchNoResults.tsx
+++ src/components/pages/search/SearchNoResults.tsx
@@ -9,14 +9,14 @@ const SearchNoResults: FC<{ bodyText: string }> = ({ bodyText }) => {
 
   return (
     <div className="flex flex-col items-center w-full xs:pl-4 lg:pl-0  bg-[#F4F7FD] my-6">
-      <div className="">
+      <div className="pt-4">
         <img src="/images/search/no-results-search.svg" />
       </div>
       <div className="text-[32px] text-primary pb-2">No results</div>
       <div className="text-secondary mb-4">{bodyText}</div>
       <a
         href={`${HELP_CENTER_LINK}en/search?${helpCenterParams}`}
-        className="flex items-center  p-4 h-10 w-fit rounded-md font-semibold bg-white hover:bg-slate-200"
+        className="flex items-center  p-4 h-10 w-fit rounded-md font-semibold bg-white hover:bg-slate-200 mb-4  "
       >
         Open Help Center
       </a>
