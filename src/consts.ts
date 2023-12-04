e3d3fa0a0c203cd6b0a1c509e144dd0068828103
Ciarán Ainsworth <ciaranainsworth@proton.me>
2023-12-04 17:20:25 +0100

    chore: update SEO image and consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE = {
-  title: "Documentation",
-  description: "Your website description.",
+  title: "Adjust developer documentation",
+  description: "Adjust developer documentation hub.",
   defaultLanguage: "en-us",
 } as const;
 
@@ -9,14 +9,9 @@ export const HELP_CENTER_LINK = "https://help.adjust.com/";
 
 export const OPEN_GRAPH = {
   image: {
-    src: "https://github.com/withastro/astro/blob/main/.github/assets/banner.png?raw=true",
+    src: "/images/adjustbackground.png",
     alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
+      "The Adjust logo on a blue background"
   },
-  twitter: "astrodotbuild",
+  twitter: "adjustcom",
 };
-
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
-
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
