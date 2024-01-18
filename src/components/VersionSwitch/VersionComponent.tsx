a94e80f6f92115a0781e1bcdd442687eb629328c
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-01-18 17:30:35 +0200

    feat: added version content to the Version component

--- src/components/VersionSwitch/VersionComponent.tsx
+++ src/components/VersionSwitch/VersionComponent.tsx
@@ -1,7 +1,25 @@
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
+import { useStore } from "@nanostores/react";
 
-const VersionComponent: FC = () => {
-  return <div>content</div>;
+import { $versions, updateVersionsItems } from "@store/versionsStore";
+
+const VersionComponent: FC<{
+  content?: JSX.Element;
+  version: string;
+}> = ({ content, version }) => {
+  const versionsStore = useStore($versions);
+
+  const versionOption = [{ label: version, value: version }];
+
+  useEffect(() => {
+    updateVersionsItems(versionOption);
+  }, []);
+
+  if (version !== versionsStore.currentVersion.value) {
+    return null;
+  }
+
+  return <div className="py-4">{content}</div>;
 };
 
 export default VersionComponent;
