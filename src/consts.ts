9cd99292003672918c4adc80f119a4c7dd960882
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-02-08 16:43:08 +0200

    fix: fixed problem with content matching

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ export const SITE = {
   title: "Adjust developer documentation",
   description: "Adjust developer documentation hub.",
   defaultLanguage: "en-us",
+  defaultLanguageKey: "en",
 } as const;
 
 export const CONTENT_PATH = "src/content/docs";
@@ -10,8 +11,7 @@ export const HELP_CENTER_LINK = "https://help.adjust.com/";
 export const OPEN_GRAPH = {
   image: {
     src: "/images/adjustbackground.png",
-    alt:
-      "The Adjust logo on a blue background"
+    alt: "The Adjust logo on a blue background",
   },
   twitter: "adjustcom",
 };
