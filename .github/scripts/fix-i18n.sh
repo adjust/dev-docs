a4bede70f280ac2fc3925180ad92ff0b5d168bb5
Ciarán Ainsworth <cda@sporiff.dev>
2024-04-10 15:06:09 +0200

    Update fixes for version switcher

--- .github/scripts/fix-i18n.sh
+++ .github/scripts/fix-i18n.sh
@@ -17,27 +17,23 @@ for locale do
    find 'src/content/docs/' -name "*-$locale.mdx" -type f -exec sh -c \
       'for file do \echo '\''Fixing formatting for '"${locale}"' content'\''; \
       sed -i -e "s/<abbr/<Abbr/g ; \
-      s/[^\n]<accordion/\n\n<Accordion/g ; \
       s/<accordion/<Accordion/g ; \
-      s/[^\n]<callout/\n\n<Callout/g ; \
+      s/<apiversion/<ApiVersion/g ; \
+      s/<sdkversion/<SdkVersion/g ; \
       s/<callout/<Callout/g ; \
-      s/[^\n]<minorversion/\n\n<MinorVersion/g ; \
       s/<minorversion/<MinorVersion/g ; \
-      s/[^\n]<table/\n\n<Table/g ; \
       s/<table/<Table/g ; \
-      s/[^\n]<tabs/\n\n<Tabs/g ; \
       s/<tabs/<Tabs/g ; \
-      s/[^\n]<\/Tabs/\n<\/Tabs/g ; \
-      s/[^\n]<tab/\n<Tab/g ; \
       s/<tab/<Tab/g ; \
-      s/[^\n]<tile/\n\n<Tile/g ; \
       s/<tile/<Tile/g" "${file}"; \
-      sed -zri -e  "s/([^\n]\n)(<\/Callout)/\1\n\2/g ; \
-      s/([^\n]\n)(<Callout)/\1\n\2/g ; \
-      s/([^\n]\n)(<\/Tile)/\1\n\2/g ; \
-      s/([^\n]\n)(<\/Tab>)/\1\n\2/g ; \
-      s/([^\n]\n)(<\/Accordion)/\1\n\2/g ; \
-      s/([^\n]\n)(<\/MinorVersion)/\1\n\2/g" "${file}"; \
+      sed -zri -e "s/((.)\s|(.)\n)(<Callout|<\/Callout)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<ApiVersion|<\/ApiVersion)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<SdkVersion|<\/SdkVersion)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<Tile|<\/Tile)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<Tabs|<\/Tabs)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<Tab|<\/Tab)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<Accordion|<\/Accordion)/\2\n\n\4/mg ; \
+      s/((.)\s|(.)\n)(<MinorVersion|<\/MinorVersion)/\2\n\n\4/mg" "${file}"; \
       echo "Updating slugs for '"${locale}"' content"; \
       sed -i -E "s/(slug:)( \"| )(en\/)/\1\2'"${locale}\/"'/g" "${file}"; \
       echo "Updating URLs for '"${locale}"' content"; \
