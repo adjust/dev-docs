44414ce58d015b2e1102686683dd05059dfc871f
Ciarán Ainsworth <cda@sporiff.dev>
2024-04-20 15:10:54 +0200

    Add plugins for version placeholder overwrites

---
+++ src/integrations/remarkReplaceVersions.ts
@@ -0,0 +1,26 @@
+import type { Plugin } from 'unified';
+import type { Node } from 'unist';
+import type { Code } from 'mdast';
+import { visit } from 'unist-util-visit';
+
+const replaceTemplateStringsPlugin: Plugin<[VersionMap]> = (options) => {
+   const versions = options || {};
+
+   return (tree: Node | undefined) => {
+      if (!tree) return;
+      visit(tree, 'code', (node: Code) => {
+         if (!node || !node.value || typeof node.value !== 'string') return;
+
+         let newValue = node.value;
+         for (const platform in versions) {
+            const replacementValue = versions[platform];
+            const oldValue = `$${platform.toUpperCase()}_VERSION`
+            newValue = newValue.replaceAll(oldValue, replacementValue);
+         }
+         node.value = newValue;
+      });
+   };
+};
+
+
+export default replaceTemplateStringsPlugin as Plugin;
