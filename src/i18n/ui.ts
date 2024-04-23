eeab9832c0d6cb24936f354b83f22f455f2db221
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-04-23 18:01:52 +0300

    feat: added basic setup for the UI strings

---
+++ src/i18n/ui.ts
@@ -0,0 +1,11 @@
+import en from "./translations/en.json";
+import ja from "./translations/ja.json";
+import ko from "./translations/ko.json";
+import zh from "./translations/zh.json";
+
+export const ui = {
+  en,
+  ja,
+  ko,
+  zh,
+} as const;
