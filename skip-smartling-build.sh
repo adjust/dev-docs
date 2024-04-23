2def7562c55c917bd94a742e160644615c1f8a15
Ciarán Ainsworth <cda@sporiff.dev>
2024-04-23 15:08:57 +0200

    Add check for diff

--- skip-smartling-build.sh
+++ skip-smartling-build.sh
@@ -4,14 +4,23 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 AUTHOR=$(git show $(git log --format="%H" -n 1) | grep Author)
 
-echo "$AUTHOR"
+echo "Checking for changes"
 
-if [[ "$AUTHOR" != "Author: adjust-pc-team <153073226+adjust-pc-team@users.noreply.github.com>" ]]
+GITDIFF=$(git diff HEAD^ HEAD --quiet; echo $?)
+
+if [[ "$GITDIFF" = 0 ]]
 then
-   echo "Building site"
-   exit 1;
-else
+   # Don't build
+   echo "No changes found. Skipping build."
+   exit 0
+fi
+
+if [[ "$AUTHOR" = "Author: adjust-pc-team <153073226+adjust-pc-team@users.noreply.github.com>" ]]
+then
    # Don't build
    echo "Skipping build from Smartling pending fixes"
    exit 0;
+else
+   echo "Building site"
+   exit 1;
 fi
