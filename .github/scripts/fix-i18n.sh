c7ff32fc033ec42fb1942da2e0699f6ba1fc37b7
Ciarán Ainsworth <cda@sporiff.dev>
2024-05-27 16:20:33 +0200

    Improve locale fix script

--- .github/scripts/fix-i18n.sh
+++ .github/scripts/fix-i18n.sh
@@ -10,33 +10,35 @@
 
 set -- ja ko zh
 
-for locale do
+for locale; do
+    find 'src/content/docs/' -name "*-$locale.mdx" -type f -exec sh -c '
+        process_file() {
+            file="$1"
+            locale="$2"
+            
+            echo "Fixing formatting for ${locale} content"
+            sed -i "" -e "
+                s/<abbr/<Abbr/g ;
+                s/<accordion/<Accordion/g ;
+                s/<apiversion/<ApiVersion/g ;
+                s/<sdkversion/<SdkVersion/g ;
+                s/<callout/<Callout/g ;
+                s/<minorversion/<MinorVersion/g ;
+                s/<table/<Table/g ;
+                s/<tabs/<Tabs/g ;
+                s/<tab/<Tab/g ;
+                s/<tile/<Tile/g ;
+            " "$file"
 
-   # Fix broken MDX components in all files
+            echo "Updating slugs for ${locale} content"
+            sed -i "" -e "s/\(slug:\)\( \"\| \)\(en\/\)/\1\2${locale}\//g" "$file"
 
-   find 'src/content/docs/' -name "*-$locale.mdx" -type f -exec sh -c \
-      'for file do \echo '\''Fixing formatting for '"${locale}"' content'\''; \
-      sed -i -e "s/<abbr/<Abbr/g ; \
-      s/<accordion/<Accordion/g ; \
-      s/<apiversion/<ApiVersion/g ; \
-      s/<sdkversion/<SdkVersion/g ; \
-      s/<callout/<Callout/g ; \
-      s/<minorversion/<MinorVersion/g ; \
-      s/<table/<Table/g ; \
-      s/<tabs/<Tabs/g ; \
-      s/<tab/<Tab/g ; \
-      s/<tile/<Tile/g" "${file}"; \
-      sed -zri -e "s/((.)\s|(.)\n)(<Callout|<\/Callout)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<ApiVersion|<\/ApiVersion)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<SdkVersion|<\/SdkVersion)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<Tile|<\/Tile)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<Tabs|<\/Tabs)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<Tab|<\/Tab)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<Accordion|<\/Accordion)/\2\n\n\4/mg ; \
-      s/((.)\s|(.)\n)(<MinorVersion|<\/MinorVersion)/\2\n\n\4/mg" "${file}"; \
-      echo "Updating slugs for '"${locale}"' content"; \
-      sed -i -E "s/(slug:)( \"| )(en\/)/\1\2'"${locale}\/"'/g" "${file}"; \
-      echo "Updating URLs for '"${locale}"' content"; \
-      sed -i -E "s/\/en\//\/'"${locale}"'\//g" "${file}"; done' none {} +
-
+            echo "Updating URLs for ${locale} content"
+            sed -i "" -e "s/\/en\//\/${locale}\//g" "$file"
+        }
+        
+        for file do
+            process_file "$file" "$0"
+        done
+    ' "$locale" {} +
 done
