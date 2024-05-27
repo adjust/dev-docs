3f1443558c6386a4e2e2da1479ae56b343367607
Ciarán Ainsworth <cda@sporiff.dev>
2024-05-27 17:40:15 +0200

    Use GNU sed syntax

--- .github/scripts/fix-i18n.sh
+++ .github/scripts/fix-i18n.sh
@@ -17,7 +17,7 @@ for locale; do
             locale="$2"
             
             echo "Fixing formatting for ${locale} content"
-            sed -i "" -e "
+            sed -i -e "
                 s/<abbr/<Abbr/g ;
                 s/<accordion/<Accordion/g ;
                 s/<apiversion/<ApiVersion/g ;
@@ -31,16 +31,16 @@ for locale; do
             " "$file"
 
             # Insert newline before <Tabs> and <Tab> without leading spaces
-            sed -i "" -e "s/[ \t]*<Tabs>/<Tabs>\n/g" -e "s/[ \t]*<Tab>/<Tab>\n/g" "$file"
+            sed -i -e "s/[ \t]*<Tabs>/<Tabs>\n/g" -e "s/[ \t]*<Tab>/<Tab>\n/g" "$file"
 
             # Insert newline after </Tab> and </Tabs> without leading spaces
-            sed -i "" -e "s/<\/Tab>[ \t]*/<\/Tab>\n/g" -e "s/<\/Tabs>[ \t]*/<\/Tabs>\n/g" "$file"
+            sed -i -e "s/<\/Tab>[ \t]*/<\/Tab>\n/g" -e "s/<\/Tabs>[ \t]*/<\/Tabs>\n/g" "$file"
 
             echo "Updating slugs for ${locale} content"
-            sed -i "" -E -e "s/(slug: *)(\"?en\/?)/\1\"${locale}\//g" "$file"
+            sed -i -E -e "s/(slug: *)(\"?en\/?)/\1\"${locale}\//g" "$file"
 
             echo "Updating URLs for ${locale} content"
-            sed -i "" -e "s/\/en\//\/${locale}\//g" "$file"
+            sed -i -e "s/\/en\//\/${locale}\//g" "$file"
         }
         
         for file do
