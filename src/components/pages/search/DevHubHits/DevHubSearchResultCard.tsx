aaf84f1209f39c32f4b16b3cbf2d5070d582d08b
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-05-28 14:56:13 +0300

    feat: replaced Highlight component with Snippet

--- src/components/pages/search/DevHubHits/DevHubSearchResultCard.tsx
+++ src/components/pages/search/DevHubHits/DevHubSearchResultCard.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 import classNames from "classnames";
-import { Highlight } from "react-instantsearch";
+import { Snippet } from "react-instantsearch";
 
 import type { DevHubSearchResultCardProps, HitBreadcrumb } from "./types";
 
@@ -35,7 +35,7 @@ const DevHubSearchResultCard: FC<DevHubSearchResultCardProps> = ({ hit }) => {
           {hit.title}
         </h5>
         <p className="text-heading-5 h-[40px] text-ellipsis overflow-hidden text-search-primary">
-          <Highlight hit={hit} attribute="content" />
+          <Snippet hit={hit} attribute="content" />
         </p>
       </a>
     </div>
