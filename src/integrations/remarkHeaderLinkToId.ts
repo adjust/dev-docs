9caffee4bebea6f14fd00973e78ac6c89acdd4f0
Ciarán Ainsworth <cda@sporiff.dev>
2024-06-13 00:23:49 +0200

    Improve header link script

--- src/integrations/remarkHeaderLinkToId.ts
+++ src/integrations/remarkHeaderLinkToId.ts
@@ -3,28 +3,33 @@ import { visit } from "unist-util-visit";
 import type { Heading, Link, Text } from "mdast";
 import type { Node } from "unist";
 
-const isLinkWithSingleTextChild = (node: Node): node is Link =>
-   (node as Link).type === "link" &&
-   (node as Link).children.length === 1 &&
-   (node as Link).children[0].type === "text";
+function isLinkWithSingleTextChild(node: Node): node is Link {
+   return (
+      node.type === "link" &&
+      (node as Link).children.length === 1 &&
+      (node as Link).children[0].type === "text"
+   );
+}
 
 const remarkHeaderLinkToId: Plugin<[]> = function () {
    return (tree: Node) => {
-      visit(tree, "heading", (node: Heading) => {
-         const firstChild = node.children[0];
-         if (node.children.length === 1 && isLinkWithSingleTextChild(firstChild)) {
-            const linkNode = firstChild as Link;
+      visit(tree, "heading", (node: Node) => {
+         const headingNode = node as Heading;
+         const firstChild = headingNode.children[0];
+         if (
+            headingNode.children.length === 1 &&
+            isLinkWithSingleTextChild(firstChild)
+         ) {
+            const linkNode = firstChild;
             const textNode = linkNode.children[0] as Text;
 
             const linkText = textNode.value;
             const linkUrl = linkNode.url;
 
             if (linkUrl && linkText) {
-               node.children = [{ type: "text", value: linkText }];
-               if (!node.data) {
-                  node.data = {};
-               }
-               node.data.hProperties = { id: linkUrl };
+               headingNode.children = [{ type: "text", value: linkText }];
+               headingNode.data ??= {};
+               headingNode.data.hProperties = { id: linkUrl };
             }
          }
       });
