00f93285c6b64a299800adc178db30453c6b1575
Ciarán Ainsworth <cda@sporiff.dev>
2024-06-13 00:19:43 +0200

    Improve version update

--- src/integrations/remarkReplaceVersions.ts
+++ src/integrations/remarkReplaceVersions.ts
@@ -4,32 +4,32 @@ import type { Code } from 'mdast';
 import { visit } from 'unist-util-visit';
 
 const replaceTemplateStringsPlugin: Plugin<[VersionMap]> = (options) => {
-   const versionRegex = /\$.*?\_VERSION/g
+   const versionRegex = /\$.*?\_VERSION/g;
    const versions = options || {};
 
    return (tree: Node | undefined) => {
       if (!tree) return;
+
       visit(tree, 'code', (node: Code) => {
-         if (!node || !node.value || typeof node.value !== 'string' || !node.value.match(versionRegex)) return;
+         if (!node.value || typeof node.value !== 'string' || !node.value.match(versionRegex)) return;
 
          let newValue = node.value;
 
-         for (const platform in versions) {
-            const currentPlatform = versions[platform];
-
+         for (const [platform, currentPlatform] of Object.entries(versions)) {
             if (typeof currentPlatform === 'string') {
                const replacementValue = currentPlatform.substring(1); // Remove leading "v"
-               const oldValue = `$${platform.toUpperCase()}_VERSION`;
-               newValue = newValue.replaceAll(oldValue, replacementValue);
+               const oldValue = new RegExp(`\\$${platform.toUpperCase()}_VERSION`, 'g');
+               newValue = newValue.replace(oldValue, replacementValue);
             } else {
                const v4ReplacementValue = currentPlatform.v4.substring(1); // Remove leading "v"
                const v5ReplacementValue = currentPlatform.v5.substring(1); // Remove leading "v"
-               const v4OldValue = `$${platform.toUpperCase()}_V4_VERSION`;
-               const v5OldValue = `$${platform.toUpperCase()}_V5_VERSION`;
-               newValue = newValue.replaceAll(v4OldValue, v4ReplacementValue);
-               newValue = newValue.replaceAll(v5OldValue, v5ReplacementValue);
+               const v4OldValue = new RegExp(`\\$${platform.toUpperCase()}_V4_VERSION`, 'g');
+               const v5OldValue = new RegExp(`\\$${platform.toUpperCase()}_V5_VERSION`, 'g');
+               newValue = newValue.replace(v4OldValue, v4ReplacementValue);
+               newValue = newValue.replace(v5OldValue, v5ReplacementValue);
             }
          }
+
          node.value = newValue;
       });
    };
