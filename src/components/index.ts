3c3810182a301824389643859e3cb47e05482cbc
Ciarán Ainsworth <cda@sporiff.dev>
2024-06-18 15:46:53 +0200

    Add API explorer example

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as ApiVersion } from "./ApiVersion.astro";
 export { default as Accordion } from "./Accordion.astro";
 export { default as Callout } from "./Callout.astro";
 export { default as CodeBlock } from "./CodeBlock.astro";
+export { default as Explorer } from "./Explorer.astro";
 export { default as FigmaEmbed } from "./FigmaEmbed/FigmaEmbed.astro";
 export { default as ListColumns } from "./ListColumns.astro";
 export { default as MinorVersion } from "./MinorVersion.astro";
