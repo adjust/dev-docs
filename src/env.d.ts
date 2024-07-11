6df2d8aa12206aefd10be6554eb552c742e036f5
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-07-11 12:46:28 +0300

    feat: updated Typesense keys

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,12 +3,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_GITHUB_TOKEN: string | undefined;
-  readonly HELP_CENTER_TYPESENSE_HOST: string;
-  readonly HELP_CENTER_TYPESENSE_API_KEY: string;
+  readonly TYPESENSE_HOST: string;
+  readonly TYPESENSE_API_KEY: string;
   readonly HELP_CENTER_TYPESENSE_INDEX_NAME: string;
   readonly HELP_CENTER_TYPESENSE_SEARCH_PRESET: string;
-  readonly DEV_HUB_TYPESENSE_HOST: string;
-  readonly DEV_HUB_TYPESENSE_API_KEY: string;
   readonly DEV_HUB_TYPESENSE_INDEX_NAME: string;
   readonly DEV_HUB_TYPESENSE_SEARCH_PRESET: string;
 }
