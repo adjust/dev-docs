89620268a759b0efa8887354c8d0058e72969ab8
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-07-19 16:28:58 +0300

    feat: added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import AutoImport from "astro-auto-import";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
+import vercel from "@astrojs/vercel/serverless";
 import remarkReplaceVersions from "./src/integrations/remarkReplaceVersions";
 import { fetchVersions } from "./src/integrations/fetchSdkVersions";
 import rehypeExternalLinks from "rehype-external-links";
@@ -52,6 +53,7 @@ export default defineConfig({
   ],
   site: "https://dev-docs-git-feat-thc-1023-update-locale-10efbe-product-content.vercel.app/",
   output: "server",
+  adapter: vercel(),
   i18n: {
     defaultLocale: "en",
     locales: ["ja", "ko", "zh", "en"],
