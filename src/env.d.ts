0a230092f51b1b6e4b141e0398ae29f25aad5434
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-07-21 22:59:11 +0300

    feat: edge functions

--- src/env.d.ts
+++ src/env.d.ts
@@ -14,3 +14,11 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+type EdgeLocals = import("@astrojs/vercel").EdgeLocals;
+
+declare namespace App {
+  interface Locals extends EdgeLocals {
+    // ...
+  }
+}
