257770eef0f69ede0f056708bbdd87ab59015a81
Bohdan Zavhorodskyi <bohdan.zavhorodskyi@kitrum.com>
2024-07-22 00:47:50 +0300

    fix: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { defineMiddleware } from "astro:middleware";
+
+import { defaultLang } from "@i18n/locales";
+import { langParamRegex } from "@i18n/utils";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const response = await next();
+
+  const path = context.url.pathname;
+
+  if (
+    response.status === 404 &&
+    langParamRegex.test(path) &&
+    !path.includes(`/${defaultLang}`)
+  ) {
+    const updatedPath = path.replace(langParamRegex, `/${defaultLang}/`);
+
+    return Response.redirect(new URL(updatedPath, context.url), 302);
+  }
+
+  return next();
+});
