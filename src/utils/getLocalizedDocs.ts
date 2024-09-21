26eb535f8603f433d901d40e066e9f88eb2bb83f
Ciarán Ainsworth <cda@sporiff.dev>
2024-09-21 17:57:23 +0200

    Improve getStaticPaths

---
+++ src/utils/getLocalizedDocs.ts
@@ -0,0 +1,34 @@
+/*
+Since our content collection isn't properly split up by language, we instead need to split it up by getting
+the language code from the beginning of the slug.
+*/
+
+import { KNOWN_LANGUAGE_CODES } from "@i18n/locales";
+import { getCollection } from "astro:content";
+
+const getLocalizedDocs = async () => {
+   const docs = await getCollection("docs")
+
+   const defaultLangFallback = docs.filter((doc) =>
+      doc.slug.startsWith("en/")
+   );
+
+   return KNOWN_LANGUAGE_CODES.flatMap((langKey) => {
+      const docsByLang = docs.filter((doc) => doc.slug.startsWith(`${langKey}/`));
+
+      // Add English pages only if they don't exist in the target language
+      const usedArray = docsByLang.length
+         ? docsByLang
+         : defaultLangFallback.map((doc) => ({
+            ...doc,
+            slug: `${langKey}/${doc.slug}`,
+         }));
+
+      return usedArray.map((doc) => ({
+         ...doc
+      }));
+   });
+}
+
+// Call the function to get the localized docs
+export const localizedDocs = await getLocalizedDocs();
