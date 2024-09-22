05f637e016cd0b6cf66d6d46edd8c22931355b9a
Ciarán Ainsworth <cda@sporiff.dev>
2024-09-22 16:38:44 +0200

    Make language tree build synchronous

--- src/utils/helpers/navigation/buildLanguageTree.ts
+++ src/utils/helpers/navigation/buildLanguageTree.ts
@@ -1,37 +1,20 @@
-/**
- * Creates a tree of content for each supported language. This is used for the
- * sidebar as well as for helper functions that query the collection.
- */
-
 import { buildSidebarHierarchy } from "@utils/helpers/navigation/buildSidebarHierarchy";
 import { localizedDocs } from "@utils/helpers/navigation/getLocalizedDocs";
 import { Languages, type ContentCollectionEntry, type LanguageTrees, type SidebarItem } from "@utils/helpers/navigation/types";
 
 /**
- *
+ * Creates a tree of content for each supported language with a queryable map.
+ * This is used for the sidebar as well as for helper functions that query the collection.
  * @param localizedDocs
- * @returns A language tree for all languages containing the polyfilled content arrays from the fetch step
+ * @returns A language tree for all languages containing the polyfilled content arrays from the fetch step.
+ * @returns A map of all content entries searchable by slug. Used for building breadcrumbs.
  */
-
-const buildLanguageTree = async (localizedDocs: ContentCollectionEntry[]): Promise<[LanguageTrees, Map<string, SidebarItem>]> => {
-   // Initialize a language tree
+const buildLanguageTree = (localizedDocs: ContentCollectionEntry[]): [LanguageTrees, Map<string, SidebarItem>] => {
    let languageTree: LanguageTrees = {
-      "en": {
-         sdk: [],
-         api: []
-      },
-      "ja": {
-         sdk: [],
-         api: []
-      },
-      "ko": {
-         sdk: [],
-         api: []
-      },
-      "zh": {
-         sdk: [],
-         api: []
-      },
+      "en": { sdk: [], api: [] },
+      "ja": { sdk: [], api: [] },
+      "ko": { sdk: [], api: [] },
+      "zh": { sdk: [], api: [] },
    };
 
    const languages = Object.values(Languages);
@@ -47,10 +30,11 @@ const buildLanguageTree = async (localizedDocs: ContentCollectionEntry[]): Promi
       const [hierarchy, slugMap] = buildSidebarHierarchy(content);
       languageTree[language] = hierarchy;
 
+      // Merge slugMap into fullSlugMap
       fullSlugMap = new Map([...fullSlugMap, ...slugMap]);
    });
 
    return [languageTree, fullSlugMap];
 };
 
-export const [languageTree, slugMap] = await buildLanguageTree(localizedDocs);
+export const [languageTree, slugMap] = buildLanguageTree(localizedDocs);
