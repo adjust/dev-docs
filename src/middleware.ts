569d63f58c16e90fc4ad75aac91ffffb93c5b89d
Ciarán Ainsworth <cda@sporiff.dev>
2024-10-10 11:02:49 +0200

    Update middleware to handle locales

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
-export const onRequest = async (context, next) => {
+import { defineMiddleware } from "astro/middleware";
+
+const languages = ["en", "ja", "ko", "zh"]
+
+export const onRequest = defineMiddleware(async (context, next) => {
    const response = await next();
+   const pathLang = context.url.pathname.split("/")[1]
    if (response.status === 404) {
-      return context.rewrite(`/en/404`);
+      const lang = languages.includes(pathLang) ? pathLang : "en";
+      return context.rewrite(`/${lang}/404`);
    }
    return response;
-}
+})
