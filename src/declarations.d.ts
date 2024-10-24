33b964d5a3e8410192032445c996723a5ff2acc0
Ciarán Ainsworth <cda@sporiff.dev>
2024-10-24 12:00:45 +0200

    Remove Tailwind from callouts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,17 +1,4 @@
-// Declare a type that matches the type used in the Banner package.
-// All banners must be one of these types. See the Banner documentation.
-// https://atlas.adeven.com/docs/components/Banner
-
 import type { MarkdownHeading } from "astro";
-
-declare type BannerKind =
-  | "neutral"
-  | "positive"
-  | "warning"
-  | "negative"
-  | "primary";
-
-declare type CalloutType = "info" | "note" | "tip" | "warning" | "important" | "seealso";
 
 declare interface ApiObject {
   orgName: string;
@@ -28,15 +15,20 @@ declare interface VersionProps {
 
 declare interface VersionMap {
   [key: string]: string | {
-    v4: string,
-    v5: string,
-  }
+    v4: string;
+    v5: string;
+  };
 }
 
-declare type BadgeColor = "neutral" | "negative" | "positive" | "warning" | "primary";
+declare type BadgeColor =
+  | "neutral"
+  | "negative"
+  | "positive"
+  | "warning"
+  | "primary";
 
 declare type TableHeights = number | "full-height" | undefined;
 
 declare interface MarkdownHeadingWithId extends MarkdownHeading {
-  id?: String
+  id?: String;
 }
